3b9caddc821f06416d3db0a21b33cad6ab596d41
Jodok Ole Glabasna <glabasnj@aschendorff.de>
2017-03-22 15:01:37 +0100

    Monitor Microsoft Failover Cluster with Check_MK

---
+++ agents/windows/plugins/av_failovercluster.ps1
@@ -0,0 +1,10 @@
+﻿# Jodok Ole Glabasna <glabasnj@aschendorff.de
+# 2017-03-20
+
+$status = (Get-Service ClusSvc).Status
+if ( $status -eq 'Running') {
+  Write-Host "<<<av_failovercluster:sep(44):encoding(cp1252)>>>"
+  Get-ClusterResource | Select-Object Name,State,MaintenanceMode,OwnerNode | ConvertTo-CSV -NoTypeInformation | select -skip 1 | % {$_ -replace '"',''}
+
+}
+
